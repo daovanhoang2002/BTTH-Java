7744222958d2d78ff4b245ecaed596ce238147d9
daovanhoang2002 <daovanhoang20020@gmail.com>
2022-01-17 17:11:45 +0700

    Create_Project_BTTH2

--- BTTH Java/src/App.java
+++ BTTH Java/src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        String strHoTen = "";
+        System.out.println("Dao Van Hoang");
+        strHoTen = sc.nextLine();
+        System.out.println("Xin chao" + strHoTen);
     }
 }
